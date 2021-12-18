de4202830c13d83f8f9e9c380732b58861951fe0
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-19 00:37:21 +0100

    begin wiring socket

--- src/chains/priceFeed.js
+++ src/chains/priceFeed.js
@@ -2,8 +2,8 @@ const Web3 = require('web3');
 
 class Feed {
   constructor(web3URL, contractAddress) {
-    this.web3 = new Web3(new Web3.providers.HttpProvider(web3URL));
-    this.contract = new this.web3.eth.Contract('', contractAddress);
+    const web3 = new Web3(new Web3.providers.HttpProvider(web3URL));
+    this.contract = new web3.eth.Contract('', contractAddress);
   }
 
   fetchPrice(baseAddress) {
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const Feed = require('./chains/priceFeed');
+
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const priceRecord = {};
+
+function emitPriceAtIntervals(ids) {
+  const constants = { INCREASE: 'INCREASE', DECREASE: 'DECREASE' };
+  const feed = new Feed('', '');
+  setInterval(async () => {
+    let _record = {};
+    for (const id of ids) {
+      let price = await feed.fetchPrice(id);
+      price = price / 10 ** 8;
+      let _type = constants.INCREASE;
+      let _percentage = 0;
+      if (!!priceRecord[id]) {
+        _percentage =
+          priceRecord[id] > price
+            ? ((priceRecord[id] - price) * 100) / priceRecord[id]
+            : ((price - priceRecord[id]) * 100) / price;
+      }
+      _type = price > priceRecord[id] ? constants.INCREASE : constants.DECREASE;
+      _record = { ..._record, [id]: { _type, _percentage, price } };
+    }
+    io.emit('price', { ..._record });
+  });
+}
+
+io.on('connection', (socket) => {
+  socket.on;
+});
