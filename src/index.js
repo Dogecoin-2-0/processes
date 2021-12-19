4526890e738297c5c5a02b82e805395961a03bb7
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-19 17:17:24 +0100

    update price feed file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Feed = require('./chains/priceFeed');
 const server = require('http').createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-const ethPriceRecord = {};
+const priceRecord = {};
 
 function emitPriceAtIntervalsForETH(ids) {
   const constants = { INCREASE: 'INCREASE', DECREASE: 'DECREASE' };
@@ -19,21 +19,47 @@ function emitPriceAtIntervalsForETH(ids) {
       let _percentage = 0;
       if (!!priceRecord[id]) {
         _percentage =
-          ethPriceRecord[id] > price
-            ? ((ethPriceRecord[id] - price) * 100) / ethPriceRecord[id]
-            : ((price - ethPriceRecord[id]) * 100) / price;
+          priceRecord[id] > price
+            ? ((priceRecord[id] - price) * 100) / priceRecord[id]
+            : ((price - priceRecord[id]) * 100) / price;
       } else {
-        ethPriceRecord[id] = price;
+        priceRecord[id] = price;
       }
-      _type =
-        price > ethPriceRecord[id] ? constants.INCREASE : constants.DECREASE;
+      _type = price > priceRecord[id] ? constants.INCREASE : constants.DECREASE;
       _record = { ..._record, [id]: { _type, _percentage, price } };
-      ethPriceRecord[id] = price;
+      priceRecord[id] = price;
     }
     io.emit('eth_price', { ..._record });
-  });
+  }, 5000);
+}
+
+function emitPriceAtIntervalsForBSC(ids) {
+  const constants = { INCREASE: 'INCREASE', DECREASE: 'DECREASE' };
+  const feed = new Feed('', '');
+  setInterval(async () => {
+    let _record = {};
+    for (const id of ids) {
+      let price = await feed.fetchPrice(id);
+      price = price / 10 ** 8;
+      let _type = constants.INCREASE;
+      let _percentage = 0;
+      if (!!priceRecord[id]) {
+        _percentage =
+          priceRecord[id] > price
+            ? ((priceRecord[id] - price) * 100) / priceRecord[id]
+            : ((price - priceRecord[id]) * 100) / price;
+      } else {
+        priceRecord[id] = price;
+      }
+      _type = price > priceRecord[id] ? constants.INCREASE : constants.DECREASE;
+      _record = { ..._record, [id]: { _type, _percentage, price } };
+      priceRecord[id] = price;
+    }
+    io.emit('bsc_price', { ..._record });
+  }, 5000);
 }
 
 io.on('connection', (socket) => {
   emitPriceAtIntervalsForETH(['0x']);
+  emitPriceAtIntervalsForBSC(['0x']);
 });
