88af37724e77ce82710711d8cf5e5d4e8630aa48
Kingsley Victor <javaprodigy56@gmail.com>
2022-01-22 22:30:00 +0100

    add more processes

--- src/chains/ethereum/index.js
+++ src/chains/ethereum/index.js
@@ -16,7 +16,44 @@ class EthProcesses {
     const blockInfo = await this.web3.eth.getBlock(block_id);
     try {
       if (blockInfo.transactions) {
-        for (const el of blockInfo.transactions) console.log(el);
+        for (const el of blockInfo.transactions) this.getTransactionDetail(el, block_id);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getTransactionDetail(transaction_id, block_id) {
+    const txReceipt = await this.web3.eth.getTransactionReceipt(transaction_id);
+
+    if (txReceipt !== null && typeof txReceipt.status !== 'undefined' && !txReceipt.status) {
+      console.log('Tx receipt status failed');
+      return;
+    }
+
+    try {
+      const tx = await this.web3.eth.getTransaction(transaction_id.toString());
+      let transactionDetail = {};
+
+      if (!!txReceipt.status && txReceipt.logs.length > 0) {
+        const logs = txReceipt.logs;
+        for (const log of logs) {
+          const callValue = await this.web3.eth.call({ to: log.address, data: this.web3.utils.sha3('decimals()') });
+          const isERC20 = callValue !== '0x' || callValue !== '0x0';
+          if (isERC20 && log.topics[1] !== undefined && log.topics[2] !== undefined) {
+            const contract = new this.web3.eth.Contract(null, log.address);
+            const decimals = await contract.methods.decimals().call();
+            transactionDetail = {
+              ...transactionDetail,
+              tx_id: log.transactionHash,
+              from: tx.from,
+              to: tx.to,
+              block_id: log.blockNumber,
+              amount: log.data / decimals,
+              is_erc_20: true
+            };
+          }
+        }
       }
     } catch (error) {
       console.log(error);
