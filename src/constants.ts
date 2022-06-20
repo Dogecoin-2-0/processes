5a20fe34557568f6344da9e38bd5cc936ffbfa94
Kingsley Victor <javaprodigy56@gmail.com>
2022-06-20 23:35:07 +0100

    ...

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const redisLastProcessedBlockKey = 'last_processed_block_:chainIdHex:';
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,27 +3,35 @@ import { getAddress } from '@ethersproject/address';
 import { hexValue } from '@ethersproject/bytes';
 import { formatEther, formatUnits } from '@ethersproject/units';
 import { find, forEach, map, any as anyMatch, multiply } from 'ramda';
+import redis from '../helpers/redis';
 import rpcCall from '../utils/rpc';
 import chainlist from '../chainlist.json';
 import erc20Abi from '../assets/ERC20ABI.json';
 import { TransactionSchema } from '../objects/schemas';
 import * as db from '../db';
 import log from '../log';
+import { redisLastProcessedBlockKey } from '../constants';
 
-export function watchBlockChanges(blockNumber: number, chainId: number) {
+export function propagateBlockData(blockNumber: number, chainId: number) {
   return async () => {
     const chain = find(c => c.id === chainId, chainlist);
 
     if (!chain) throw new Error('invalid chain');
 
     const blockNumberAsHex = hexValue(blockNumber);
+
+    log('Now reading block %s on chain %s', blockNumberAsHex, hexValue(chainId));
+
     const blockResult: { transactions: Array<TransactionSchema>; timestamp: string } = await rpcCall(chain.rpcUrl, {
       method: 'eth_getBlockByNumber',
       params: [blockNumberAsHex, true]
     });
 
     forEach(transaction => {
-      const { from, to, value, hash, input } = transaction;
+      const { from, to, value, hash } = transaction;
+
+      log('Now reading transaction: ', hash);
+
       (async () => {
         const abiInterface = new Interface(erc20Abi);
         const data = abiInterface.getSighash('decimals()');
@@ -63,6 +71,8 @@ export function watchBlockChanges(blockNumber: number, chainId: number) {
             log('New transaction stored: %s', JSON.stringify(tx, undefined, 2));
           }
         } else {
+          log('Now calling eth_getLogs');
+
           const logs = await rpcCall(chain.rpcUrl, {
             method: 'eth_getLogs',
             params: [{ fromBlock: blockNumberAsHex, toBlock: blockNumberAsHex, address: to }]
@@ -120,5 +130,26 @@ export function watchBlockChanges(blockNumber: number, chainId: number) {
         }
       })();
     }, blockResult.transactions);
+
+    const val = await redis.simpleSet(
+      redisLastProcessedBlockKey.replace(':chainIdHex:', hexValue(chainId)),
+      blockNumber
+    );
+
+    log('Block data processed successfully. Redis response: ', val);
   };
+}
+
+export function syncFromLastProcessedBlock(chainId: number) {
+  (async () => {
+    const exists = await redis.exists(redisLastProcessedBlockKey.replace(':chainIdHex:', hexValue(chainId)));
+
+    if (exists) {
+      let lastProcessedBlock: any = await redis.simpleGet(
+        redisLastProcessedBlockKey.replace(':chainIdHex:', hexValue(chainId))
+      );
+      lastProcessedBlock = parseInt(lastProcessedBlock);
+      propagateBlockData(lastProcessedBlock, chainId);
+    }
+  })();
 }
