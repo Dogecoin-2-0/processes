f447ab3e50fce136ea5366f1743b9ff382451c34
Kingsley Victor <javaprodigy56@gmail.com>
2022-06-21 16:00:27 +0100

    begin wiring router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { forEach } from 'ramda';
+import { getAddress } from '@ethersproject/address';
+import { forEach, find, map, pick } from 'ramda';
 import * as db from './db';
 import CustomError from './custom/error';
 import _redis from './helpers/redis';
@@ -10,7 +11,7 @@ import log from './log';
 
 const app = express();
 const router = express.Router();
-const port = parseInt(process.env.PORT || '18000');
+const port = parseInt(process.env.PORT || '3600');
 
 function watchBlocks() {
   forEach(chain => {
@@ -20,6 +21,25 @@ function watchBlocks() {
     provider.on('block', (blockNumber: number) => propagateBlockData(blockNumber, chain.id)());
   }, chainlist);
 }
+
+router.post('/wallet', async (req, res) => {
+  try {
+    const { body } = pick(['body'], req);
+    const allWallets = await db.models.wallet.findWallets();
+    const allWalletsJson = map(wallet => wallet.toJSON(), allWallets);
+
+    let result: any = find(wallet => getAddress(wallet.address) === getAddress(body.address), allWalletsJson);
+
+    if (typeof result !== 'undefined') {
+      return res.status(200).json({ result });
+    }
+    result = await db.models.wallet.addWallet({ address: body.address });
+    result = result.toJSON();
+    return res.status(201).json({ result });
+  } catch (err: any) {
+    return res.status(500).json({ error: err.message });
+  }
+});
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -31,6 +51,7 @@ app.use((req, res, next) => {
 app.use('/', router);
 
 app.listen(port, () => {
+  log('Server listening on %d', port);
   db.sequelize.sync({}).then(() => {
     log('Sequelize connected to DB');
     _redis._initConnection().then(() => {
